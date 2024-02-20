724ea4ff0e58e90735e5c23450f10e254ecd4c7c
= <=>
2024-02-20 20:44:35 -0300

    Image feature terminada!

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,29 +4,25 @@ export const Homepage = () => {
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "center" }}>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
+      <ImageContainer imgSrc="https://i.pinimg.com/564x/2a/24/f1/2a24f1075ea35807be8835ebae917ac1.jpg"></ImageContainer>
+      <ImageContainer imgSrc="https://i.pinimg.com/564x/60/90/74/609074450a366e336d732d54efc26827.jpg"></ImageContainer>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
+      <ImageContainer  imgSrc="https://i.pinimg.com/236x/ba/db/73/badb73ac57cc285ea3a38c3207eee8a3.jpg"></ImageContainer>
+      <ImageContainer  imgSrc="https://i.pinimg.com/236x/49/0c/06/490c06a24aced8770531c3ba885717d2.jpg"></ImageContainer>
+      <ImageContainer  imgSrc="https://i.pinimg.com/236x/f4/48/83/f448832c7979826844deae6d8a266284.jpg"></ImageContainer>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
-      <MouseProvider></MouseProvider>
+      <ImageContainer  imgSrc="https://i.pinimg.com/236x/29/6b/0d/296b0de158fb5a8bf7e69287dcc90e7a.jpg"></ImageContainer>
+      <ImageContainer  imgSrc="https://i.pinimg.com/564x/e0/11/a6/e011a69638c1e099db8846e513b8b24a.jpg"></ImageContainer>
+
       </div>
     </div>
   );
 };
 
-const MouseProvider = () => {
-  const [globalPosition, setGlobalPosition] = useState({ x: 0, y: 0 });
+const ImageContainer = ({imgSrc}) => {
+
   const [localPosition, setLocalPosition] = useState({ x: 0, y: 0 });
   const containerRef = useRef(null);
   const imageRef = useRef(1);
@@ -36,7 +32,6 @@ const MouseProvider = () => {
       const { clientX, clientY } = e;
       const { left, top } = containerRef.current.getBoundingClientRect();
 
-      setGlobalPosition({ x: clientX, y: clientY });
       setLocalPosition({
         x: Math.min(
           Math.max(clientX - left, imageRef.current.offsetWidth / 2),
@@ -60,100 +55,23 @@ const MouseProvider = () => {
     };
   }, []);
 
-  const calculatePercentage = (position, containerSize) => {
-    return {
-      x: ((position.x / containerSize.width) * 100).toFixed(2),
-      y: ((position.y / containerSize.height) * 100).toFixed(2),
-    };
-  };
-
-  const containerSize = containerRef.current
-    ? containerRef.current.getBoundingClientRect()
-    : { width: 0, height: 0 };
-
-  const globalPercentage = calculatePercentage(globalPosition, {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-  const localPercentage = calculatePercentage(localPosition, containerSize);
 
   return (
-    <div style={{ minWidth: "400px" }}>
-      {/* <div>
-        Pos (Global): X{globalPosition.x} Y{globalPosition.y}
-      </div>
-      <div>
-        Pos porcentual  (Global): X{globalPercentage.x}%, <br /> Y global:{" "}
-        {globalPercentage.y}%
-      </div>
-      <div>
-        Pos "local"  (Useref): X{localPosition.x}  <br /> Y 
-        {localPosition.y}
-      </div>
-    
-      <div>
-        Pos % local: {localPercentage.x}%, <br /> Y local:{" "}
-        {localPercentage.y}%
-      </div> */}
-      <div
-        ref={containerRef}
-        style={{
-          width: "100%",
-          height: "300px",
-          border: "1px solid white",
-          position: "relative",
-        }}
-      >
-        <div
+    <div ref={containerRef} className="mainConteiner" >
+      <div 
+        className="imageConteiner"
           ref={imageRef}
-          style={{
-            border: "1px solid white",
-            position: "absolute",
+          style={{  
             transform: `translate(${
               localPosition.x - imageRef.current.offsetWidth / 2}px, 
             ${localPosition.y - imageRef.current.offsetHeight / 2}px)`,
-            transition: "all 1s"
+           
           }}
         >
-          <img
-            style={{
-              height: "200px",
-            }}
-            src="https://i.pinimg.com/236x/9f/40/2e/9f402e12cffbb9c380b28a93875ae6b3.jpg"
+          <img className="imageMov"
+            src={imgSrc}
           ></img>
         </div>
-      </div>
-    </div>
-  );
-};
-
-const CenterMouseOnHover = () => {
-  const containerRef = useRef(null);
-
-  const handleMouseHover = (e) => {
-    // Calcula la posición del mouse relativa al elemento que recibe el hover
-    const rect = containerRef.current.getBoundingClientRect();
-    const offsetX = rect.width / 2;
-    const offsetY = rect.height / 2;
-    const x = rect.left + offsetX;
-    const y = rect.top + offsetY;
-
-    // Mueve el mouse a la posición calculada
-    window.scrollTo(x, y);
-  };
-
-  return (
-    <div
-      ref={containerRef}
-      style={{
-        width: "300px",
-        height: "300px",
-        backgroundColor: "lightgray",
-        position: "relative",
-      }}
-      onMouseEnter={handleMouseHover} // Activar la función cuando el mouse entra en el elemento
-    >
-      {/* Contenido del componente */}
     </div>
   );
 };
