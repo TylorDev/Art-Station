74c4b5d3487d50022f23d52f4d8cd3642579d75f
TylorDev <jhontmg26@gmail.com>
2024-02-22 11:07:28 -0300

    Feature-Tab start

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,6 +3,17 @@ export const Homepage = () => {
   return (
     <div>
       <div style={{ display: "flex", justifyContent: "center" }}>
+
+
+       <div className="tabConteiner">
+
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque tenetur quam impedit quibusdam sit doloribus ipsa reprehenderit odio cumque libero ratione et facere aliquid velit animi, adipisci esse. Ad, odio.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto enim eum tenetur et provident officiis a mollitia distinctio aliquid earum exercitationem reiciendis esse modi magni id in eaque, assumenda deserunt.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet voluptate reprehenderit tenetur quam est. Eligendi vitae porro voluptatem incidunt animi eos optio ex velit libero. Expedita vel nemo nesciunt quo?
+          </div>
+
+       </div>
       </div>
 
     </div>
