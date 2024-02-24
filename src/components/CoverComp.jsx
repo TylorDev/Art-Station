4fa1827e6293449e910e71404602d7913643476e
TylorDev <jhontmg26@gmail.com>
2024-02-23 22:01:02 -0300

    Refactor scss terminado

---
+++ src/components/CoverComp.jsx
@@ -0,0 +1,6 @@
+export function CoverComp() {
+  return <div className="unNamed">
+    <img className="Cover" src=".\src\assets\example2.png" alt="" />
+    <div className="Name"> 四宮しのみや かぐや </div>
+  </div>;
+}
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,43 +2,46 @@ import { ColiderComp } from "./ColiderComp";
 import { TabComponent } from "./TabComponent";
 import { ContentComp } from "./ContentComp";
 import { useState } from "react";
+import { CoverComp } from "./CoverComp";
 export const Homepage = () => {
   return (
-    <div  style={{display: 'flex', justifyContent:'start', width: "100%", marginTop: '5%'}}>
-          {/* <div style={{display: 'flex', flexDirection:'column', width: '50%'}}>
-                 
-                <div style={{display: 'flex'}}>
-                <ColiderComp>
-                  <TabComponent ></TabComponent>
-                  </ColiderComp>
-                  <ColiderComp>
-                  <TabComponent ></TabComponent>
-                  </ColiderComp>
-                  <ColiderComp>
-                  <TabComponent ></TabComponent>
-                  </ColiderComp>
-                </div>
-                <div style={{display: 'flex'}}>
-                <ColiderComp>
-                  <TabComponent ></TabComponent>
-                  </ColiderComp>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "start",
+        width: "100%",
+        marginTop: "5%",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column", width: "50%" }}>
+        <div style={{ display: "flex" }}>
+          <ColiderComp>
+            <TabComponent></TabComponent>
+          </ColiderComp>
+          <ColiderComp>
+            <TabComponent></TabComponent>
+          </ColiderComp>
+          <ColiderComp>
+            <TabComponent></TabComponent>
+          </ColiderComp>
+        </div>
+        <div style={{ display: "flex" }}>
+          <ColiderComp>
+            <TabComponent></TabComponent>
+          </ColiderComp>
 
-                  <ColiderComp>
-                  <TabComponent ></TabComponent>
-                  </ColiderComp>
+          <ColiderComp>
+            <TabComponent></TabComponent>
+          </ColiderComp>
 
-                  <ColiderComp>
-                  <TabComponent ></TabComponent>
-                  </ColiderComp>
-                </div>
-              
-              </div>
-              
-        <img className="Cover" src=".\src\assets\example2.png" alt="" />
-        <div className="Name"> 四宮しのみや かぐや </div> */}
-          
-     
+          <ColiderComp>
+            <TabComponent></TabComponent>
+          </ColiderComp>
+        </div>
+      </div>
+
+      <CoverComp></CoverComp>
     </div>
-   
   );
 };
+
